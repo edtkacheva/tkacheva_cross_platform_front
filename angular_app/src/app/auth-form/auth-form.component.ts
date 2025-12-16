1772755bca9317e35c5015fec4b756885a4a1226
edtkacheva <140169916+edtkacheva@users.noreply.github.com>
2025-12-16 22:47:05 +0300

    angular 19

--- angular_app/src/app/auth-form/auth-form.component.ts
+++ angular_app/src/app/auth-form/auth-form.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-auth-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './auth-form.component.html',
-  styleUrl: './auth-form.component.css'
+    selector: 'app-auth-form',
+    imports: [],
+    templateUrl: './auth-form.component.html',
+    styleUrl: './auth-form.component.css'
 })
 export class AuthFormComponent {
 
